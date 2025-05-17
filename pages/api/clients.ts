7445437f2d1cd6d43b035b0fbd5feafde6e023d8
Saketh Alli <sakethalli007@gmail.com>
2025-05-16 20:02:11 -0500

    Fix TypeScript implicit any error in convertToDDMMYYYY

--- pages/api/clients.ts
+++ pages/api/clients.ts
@@ -1,6 +1,6 @@
 import { connectToDB } from '../../utils/db';
 import Client from '../../models/Clients';
-function convertToDDMMYYYY(dateString) {
+function convertToDDMMYYYY(dateString: string) {
   const [year, month, day] = dateString.split("-");
   return `${day.padStart(2, '0')}/${month.padStart(2, '0')}/${year}`;
 }
